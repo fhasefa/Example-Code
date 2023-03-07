96cabfb31f757b913465b43e6401f52abc4af07e
Jade <jadecdrb@gmail.com>
2023-03-07 16:02:07 -0500

    reordered routes

--- Week 1/Day 2 (Tue)/express_plants/server.js
+++ Week 1/Day 2 (Tue)/express_plants/server.js
@@ -3,11 +3,23 @@ const app = express()
 
 const PORT = 8080
 
-const plants = ['Monstera Deliciosa', 'Corpse Flower', 'Elephant-Foot Yam', "Witches' Butter",];
+const plants = ['Monstera Deliciosa', 'Corpse Flower', 'Elephant-Foot Yam', "Witches' Butter"];
 
-app.get('/:index', (req, res) => {
+
+app.get('/awesome', (req, res) => { //this will never be reached
+    console.log('/awesome')
+    res.send(`
+    <h1>Plants are awesome!</h1>
+    <img src="https://static.boredpanda.com/blog/wp-content/uuuploads/plant-sculptures-mosaicultures-internationales-de-montreal/plant-sculptures-mosaicultures-internationales-de-montreal-14.jpg" >
+  `);
+});
+
+app.get('/:index', (req, res) => { //:index can be anything, even awesome
+    console.log('/:index')
     res.send(plants[req.params.index]);
 });
+
+
 
 app.listen(PORT, () => {
     console.log('Listening on port: ', PORT)
