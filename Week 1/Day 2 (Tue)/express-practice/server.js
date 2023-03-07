e95fa42aeb2e07c5ebccf9a0b784d1ac096e28c2
Jade <jadecdrb@gmail.com>
2023-03-07 15:14:57 -0500

    adding other files

---
+++ Week 1/Day 2 (Tue)/express-practice/server.js
@@ -0,0 +1,15 @@
+// Load express
+const express = require('express')
+
+// Create our express app
+const app = express()
+
+// Our "root" route for our app 
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World</h1>')
+})
+
+// Tell the app to listen on port 8080 for HTTP requests from clients
+app.listen(8080, () => {
+    console.log('Listening on port 8080')
+})
