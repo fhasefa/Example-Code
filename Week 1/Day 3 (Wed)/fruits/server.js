c30c83e0d9875deb26c67eb8c6dd166b959dba17
Jade <jadecdrb@gmail.com>
2023-03-08 14:59:46 -0500

    added other food groups and filtering

--- Week 1/Day 3 (Wed)/fruits/server.js
+++ Week 1/Day 3 (Wed)/fruits/server.js
@@ -8,79 +8,60 @@ const PORT = 8080
 
 // Load our fruit data from models folder
 const fruits = require('./models/fruits')
+const vegetables = require('./models/vegetables')
+const meats = require('./models/meats')
 
-// Setup an "index" route 
+// Setup an "index" route for all food
+app.get('/food', (req, res) => {
+    // combine all the food group arrays into one big array
+    let food = [...fruits, ...vegetables, ...meats]
+    // when there is a query for color -> "/food?color=red"
+    if (req.query.color) {
+        // make a new array only containing items that match the color from the query
+        let filtered = food.filter(item => item.color === req.query.color)
+        // send only those items back
+        res.send(filtered)
+    } else {
+    // when there are no queries -> "/food"
+        res.send([...fruits, ...vegetables, ...meats])
+        // Some examples of other ways we could've sent the data back: 
+        //      res.send([fruits, vegetables, meats].flat())
+        //      res.send([fruits, vegetables, meats])
+        //      res.send({ fruits, vegetables, meats })
+    }
+})
+
+// Setup an "index" route for fruits
 app.get('/fruits', (req, res) => {
     res.send(fruits)
 })
 
-// Setup a "show" route
+// Setup a "show" route for fruits
 app.get('/fruits/:index', (req, res) => {
     res.send(fruits[req.params.index])
+})
+
+// Setup an "index" route for vegetables
+app.get('/vegetables', (req, res) => {
+    res.send(vegetables)
+})
+
+// Setup a "show" route for vegetables
+app.get('/vegetables/:index', (req, res) => {
+    res.send(vegetables[req.params.index])
+})
+
+// Setup an "index" route for meats
+app.get('/meats', (req, res) => {
+    res.send(meats)
+})
+
+// Setup a "show" route for fruits
+app.get('/meats/:index', (req, res) => {
+    res.send(meats[req.params.index])
 })
 
 // Listen to the given port
 app.listen(PORT, () => {
     console.log('Listening to the port: ' + PORT)
 })
-
-
-
-// MORE DATA!!
-
-/*
-
-const vegetables = [
-    {
-        name:'cucumber',
-        color: 'green',
-        readyToEat: true
-    },
-    {
-        name:'tomato',
-        color: 'red',
-        readyToEat: false
-    },
-    {
-        name:'eggplant',
-        color: 'purple',
-        readyToEat: false
-    }
-];
-
-
-const meats = [
-    {
-        name:'chicken',
-        state: 'raw',
-        readyToEat: false
-    },
-    {
-        name:'beef',
-        state: 'well done',
-        readyToEat: true
-    },
-    {
-        name:'pork',
-        state: 'rare',
-        readyToEat: true
-    }
-];
-
-*/
-
-/*
-
-TASK 
-
-    PART 1
-        - Create models for vegetables and meats
-        - Create an index and show route for each
-        - Create a route for "food" that returns all food groups
-
-    PART 2 
-        - Add some logic for filtering the data based on the Query Params passed from the client
-        - HINTS (you might need the "filter" method, maybe "includes", and definitely your homie "req")
-
-*/
-
