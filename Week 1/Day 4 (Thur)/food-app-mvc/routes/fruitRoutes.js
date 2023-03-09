f2bb419b006cc47b1e20480333c2ddd86bdf1975
Jade <jadecdrb@gmail.com>
2023-03-09 15:22:26 -0500

    added acronym for induces

--- Week 1/Day 4 (Thur)/food-app-mvc/routes/fruitRoutes.js
+++ Week 1/Day 4 (Thur)/food-app-mvc/routes/fruitRoutes.js
@@ -6,10 +6,17 @@ const router = express.Router()
 // Load our controller and its route logic
 const fruitController = require('../controllers/fruitController')
 
+// I.N.D.U.C.E.S  ->  an acronym that helps remember how to properly order routes
+// Index, New, Delete, Update, Create, Edit, Show
+
 // Setup an "index" route for fruits, attach it to router along with the controller logic
 router.get('/', fruitController.index)
 
+// Setup a "new" route for creating fruit
+router.get('/new')
+
 // Setup an "show" route for fruits, attach it to router along with the controller logic
 router.get('/:index', fruitController.show)
+
 
 module.exports = router
