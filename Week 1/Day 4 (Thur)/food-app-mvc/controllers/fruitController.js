38e8618ef67cf8e95cff4f585e177e09ab88b646
Jade <jadecdrb@gmail.com>
2023-03-09 12:15:22 -0500

    added some jsx to show route and props

--- Week 1/Day 4 (Thur)/food-app-mvc/controllers/fruitController.js
+++ Week 1/Day 4 (Thur)/food-app-mvc/controllers/fruitController.js
@@ -2,12 +2,12 @@ const fruits = require('../models/fruits')
 
 // The callback functions originally the second argument from -> app.get('/', () => {})
 function index(req, res) {
-    res.send(fruits)
+    res.render('fruits/Index', { fruits })
 }
 
 // Those anonymous callback functions now have names: "index" and "show"
 function show(req, res) {
-    res.render('fruits/Show')
+    res.render('fruits/Show', { fruit: fruits[req.params.index] })
 }
 
 module.exports = { index, show }
--- Week 1/Day 4 (Thur)/food-app-mvc/views/fruits/Show.jsx
+++ Week 1/Day 4 (Thur)/food-app-mvc/views/fruits/Show.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-function Show() {
+function Show(props) {
     return (
         <div>
-            Show View
+            <h1>Show View</h1>
+            <p>The <strong>{props.fruit.name}</strong> {props.fruit.readyToEat ? 'is ready to eat' : 'is NOT ready to eat'}</p>
+            <p>It's color is {props.fruit.color}</p>
         </div>
     )
 }
