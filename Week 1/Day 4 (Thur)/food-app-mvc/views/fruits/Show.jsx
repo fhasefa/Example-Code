0be7b3d7c7262341dd3abe8489fda32c6cf19a29
Jade <jadecdrb@gmail.com>
2023-03-09 12:40:17 -0500

    added a back button

--- Week 1/Day 4 (Thur)/food-app-mvc/views/fruits/Show.jsx
+++ Week 1/Day 4 (Thur)/food-app-mvc/views/fruits/Show.jsx
@@ -6,6 +6,7 @@ function Show(props) {
             <h1>Show View</h1>
             <p>The <strong>{props.fruit.name}</strong> {props.fruit.readyToEat ? 'is ready to eat' : 'is NOT ready to eat'}</p>
             <p>It's color is {props.fruit.color}</p>
+            <a href="/fruits">Back</a>
         </div>
     )
 }
