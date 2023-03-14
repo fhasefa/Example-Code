2434e4732a3cae2f205382022d275876896c2f8c
Jade <jadecdrb@gmail.com>
2023-03-14 10:38:49 -0400

    added async function

--- Week 2/Day 2 (Tue)/food-mvc-mongo/config/db.js
+++ Week 2/Day 2 (Tue)/food-mvc-mongo/config/db.js
@@ -1,2 +1,12 @@
 const mongoose = require('mongoose')
 const db = process.env.MONGO_URI
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db, {
+            useNewUrlParser: true
+        })
+    } catch (e) {
+        
+    }
+}
