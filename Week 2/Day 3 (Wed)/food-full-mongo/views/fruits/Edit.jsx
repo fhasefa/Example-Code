8b7b27221692cac01e88f4aefe6c12bf719c73cf
Jade <jadecdrb@gmail.com>
2023-03-15 12:54:38 -0400

    changed value to defaultValue

--- Week 2/Day 3 (Wed)/food-full-mongo/views/fruits/Edit.jsx
+++ Week 2/Day 3 (Wed)/food-full-mongo/views/fruits/Edit.jsx
@@ -8,10 +8,10 @@ function Edit(props) {
             <h1>Edit Fruit</h1>
             <form action={`/fruits/${props.fruit._id}?_method=PUT`} method="POST">
                 <label htmlFor="nme">Name:</label><br />
-                <input type="text" id="nme" name="name" value={props.fruit.name} /><br /><br />
+                <input type="text" id="nme" name="name" defaultValue={props.fruit.name} /><br /><br />
 
                 <label htmlFor="clr">Color:</label><br />
-                <input type="text" id="clr" name="color" value={props.fruit.color} /><br /><br />
+                <input type="text" id="clr" name="color" defaultValue={props.fruit.color} /><br /><br />
 
                 <label htmlFor="rdy">Ready To Eat:</label>
                 <input type="checkbox" id="rdy" name="readyToEat" defaultChecked={props.fruit.readyToEat} /><br /><br />
