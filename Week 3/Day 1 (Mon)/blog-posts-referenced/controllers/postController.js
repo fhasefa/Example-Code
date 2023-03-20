464f12d1a39cb800038f235d802f55c14a2c8441
Jade <jadecdrb@gmail.com>
2023-03-20 12:43:04 -0400

    added logic for show and delete for referenced documents

--- Week 3/Day 1 (Mon)/blog-posts-referenced/controllers/postController.js
+++ Week 3/Day 1 (Mon)/blog-posts-referenced/controllers/postController.js
@@ -1,4 +1,5 @@
 const Posts = require('../models/postModel')
+const Comments = require('../models/commentModel')
 const posts = require('../models/posts')
 
 module.exports.seed = async (req, res) => {
@@ -44,8 +45,8 @@ module.exports.edit = async (req, res) => {
 }
 
 module.exports.show = async (req, res) => {
-    const post = await Posts.findById(req.params.id)
-    console.log(post)
+    // populate replaces the ids with actual documents/objects we can use
+    const post = await Posts.findById(req.params.id).populate('comments')
     res.render('posts/Show', { post })
 }
 
@@ -53,12 +54,30 @@ module.exports.show = async (req, res) => {
 // EXTRA LOGIC (for comments)
 
 module.exports.createComment = async (req, res) => {
-    // create a comment by updating the comments property in post
+    // create a document in our comments collection
+    const comment = await Comments.create(req.body)
+    // find the post 
+    await Posts.findByIdAndUpdate(req.params.id, {
+        // and push the new comment document's id
+        $push: {
+            // to the post's comments field/property
+            comments: comment._id
+        }
+    })
     res.redirect(`/posts/${req.params.id}`)
 }
 
 module.exports.deleteComment = async (req, res) => {
-    // delete a comment by updating the comments property in post
+    // first use the id to delete the comment from the comments collection
+    await Comments.findByIdAndDelete(req.params.cid)
+    // then use the post's id to find the post
+    await Posts.findByIdAndUpdate(req.params.id, {
+        // and pull/remove the reference id (to the comment) from
+        $pull: {
+            // the comments array
+            comments: req.params.cid
+        }
+    })
     res.redirect(`/posts/${req.params.id}`)
 }
 
