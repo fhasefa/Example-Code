6136374b642b1beb3bfd1046bf487d117edb88b0
Jade <jadecdrb@gmail.com>
2023-03-20 11:20:19 -0400

    added the rest of logic for comment CRUD

--- Week 3/Day 1 (Mon)/blog-posts-starter/controllers/postController.js
+++ Week 3/Day 1 (Mon)/blog-posts-starter/controllers/postController.js
@@ -54,25 +54,53 @@ module.exports.show = async (req, res) => {
 
 module.exports.createComment = async (req, res) => {
     // create a comment by updating the comments property in post
+    await Posts.findByIdAndUpdate(req.params.id, {
+        // push the req.body to the comments property/field of this post document
+        $push: {
+            comments: req.body
+        }
+    })
     res.redirect(`/posts/${req.params.id}`)
 }
 
 module.exports.deleteComment = async (req, res) => {
     // delete a comment by updating the comments property in post
+    await Posts.findByIdAndUpdate(req.params.id, {
+        // pulling out or removing a subdocument  
+        $pull: {
+            // from the comments property/field
+            comments: {
+                // with a matching comment id
+                _id: req.params.cid
+            }
+        }
+    })
     res.redirect(`/posts/${req.params.id}`)
 }
 
 module.exports.indexComment = async (req, res) => {
     // target the comments property 
-    res.send('')
+    const post = await Posts.findById(req.params.id)
+    res.send(post.comments)
 }
 
 module.exports.showComment = async (req, res) => {
     // find the post and filter it's comments property array
+    const post = await Posts.findById(req.params.id)
+    const [ comment ] = post.comments.filter(comment => comment._id.toString() === req.params.cid) 
     res.render('comments/Edit', { postId: req.params.id, comment })
 }
 
 module.exports.updateComment = async (req, res) => {
     // update a comment by updating an item in the comments property in post
+
+    // find the post with the matching id, then check that post's comments for matching comment id
+    await Posts.updateOne({ _id: req.params.id, 'comments._id': req.params.cid }, {
+        // set/replace the content 
+        $set: {
+            // of the comment at index (represented by $) and change its body property   -->    comments[1].body = 'value
+            'comments.$.body': req.body.body // req.body is the form data and req.body.body is the updated value of the comment
+        }
+    })
     res.redirect(`/posts/${req.params.id}`)
 }
