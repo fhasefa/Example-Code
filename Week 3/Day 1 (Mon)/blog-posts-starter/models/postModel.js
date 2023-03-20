dca665bfa0f89460d8d25941ecea0226607675e0
Jade <jadecdrb@gmail.com>
2023-03-20 10:18:38 -0400

    added comments field to post schema

--- Week 3/Day 1 (Mon)/blog-posts-starter/models/postModel.js
+++ Week 3/Day 1 (Mon)/blog-posts-starter/models/postModel.js
@@ -3,11 +3,26 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
 
+// We could define a comments schema like this...
+
+// const commentSchema = new Schema({
+//    body: { type: String },
+//    user: { type: String, default: 'Bob' }
+// })
+
+// and then use it in post schema...
+// comments: [commentSchema]
+
 const postSchema = new Schema({
    subject: { type: String },
    body: { type: String },
    user: { type: String, default: 'Bob' },
-   // comments field here
+   comments: [
+      {
+         body: { type: String },
+         user: { type: String, default: 'Bob' }
+      }
+   ]
 }, { timestamps: true })
 
 const Post = mongoose.model('posts', postSchema)
